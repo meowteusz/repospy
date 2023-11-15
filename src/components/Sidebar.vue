bcb92effcd749bbb550950e5a6cca4acfecdf7cc
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-11-15 17:06:02 -0600

    Sticky sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,17 +1,26 @@
 <template>
-    <div>
-        <h1>MLDS</h1>
-    </div>
+    <div class="sticky">
+        <div>
+            <h1>MLDS</h1>
+        </div>
 
-    <ul class="nav nav-pills flex-column">
-        <li class="nav-item">
-            <router-link to="/repos" class="nav-link">Repos</router-link>
-        </li>
-        <li class="nav-item">
-            <router-link to="/users" class="nav-link">Users</router-link>
-        </li>
-        <li class="nav-item">
-            <router-link to="/teams" class="nav-link">Teams</router-link>
-        </li>
-    </ul>
+        <ul class="nav nav-pills flex-column">
+            <li class="nav-item">
+                <router-link to="/repos" class="nav-link">Repos</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/users" class="nav-link">Users</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/teams" class="nav-link">Teams</router-link>
+            </li>
+        </ul>
+    </div>
 </template>
+
+<style>
+.sticky {
+    position: sticky;
+    top: 0;
+}
+</style>
