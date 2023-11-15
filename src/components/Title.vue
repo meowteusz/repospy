d53045e280ed8f4628c2a21a5d1dbee105458bc4
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-11-15 13:37:28 -0600

    Create Title Bar component

---
+++ src/components/Title.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+    title: String
+})
+</script>
+
+<template>
+    <header class="pb-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center text-body-emphasis text-decoration-none">
+            <span class="fs-4">{{ title }}</span>
+        </a>
+    </header>
+</template>
