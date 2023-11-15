67dab8b538aefda0e222bc3ae3ed18b823ef8869
Mateusz Ryczek <mateusz@u.northwestern.edu>
2023-11-15 13:37:03 -0600

    Add routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,19 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('@views/Home.vue')
+      path: '/repos',
+      name: 'repos',
+      component: () => import('@views/Repos.vue')
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: () => import('@views/Users.vue')
+    },
+    {
+      path: '/teams',
+      name: 'teams',
+      component: () => import('@views/Teams.vue')
     },
   ]
 })
